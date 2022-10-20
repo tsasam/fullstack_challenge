db77767368ac0a2090efe03386abb4511fb6bfc7
Tsiory Arisoa <t.arisoa@mail.zeus.vision>
2022-10-21 00:33:08 +0200

    Updated organisms vue page

--- resources/ts/vue/organisms.vue
+++ resources/ts/vue/organisms.vue
@@ -1,11 +1,27 @@
 <template>
 
-   
+
    <div>
 
        <h2> Organism list ({{ organisms.length }}) </h2>
 
-        <p class="todo">TODO: Complete VUE code</p>
+       <table class="pure-table pure-table-horizontal">
+           <thead>
+           <tr>
+               <th>Id</th>
+               <th>Genus</th>
+               <th>Species</th>
+           </tr>
+           </thead>
+           <tbody>
+           <tr v-for="organism of organisms" :key="organism.id">
+               <td>{{ organism.id }}</td>
+               <td>{{ organism.genus }}</td>
+               <td>{{ organism.species }}</td>
+               <td></td>
+           </tr>
+           </tbody>
+       </table>
 
    </div>
 
@@ -21,6 +37,16 @@
 
 <script lang="ts">
 
+
+/**
+ * Type of the organisms
+ */
+type OrganismsT = {
+    id: number;
+    genus: string;
+    species: string;
+}
+
 import {Vue, Component, Prop, Watch} from 'vue-property-decorator';
 import axios from 'axios';
 
@@ -28,7 +54,19 @@ import axios from 'axios';
 @Component({})
 export default class OrganismsVue extends Vue {
 
-    organisms = [];
+    organisms: OrganismsT[] = [];
+
+    mounted(){
+        // Load the organisms when the component gets mounted
+        this.loadOrganisms();
+    }
+
+    /**
+     * Receive the organisms from the api endpoint
+     */
+    async loadOrganisms(){
+        this.organisms = (await axios.get('/api/organisms/')).data.data;
+    }
 
 }
 </script>
---
+++ resources/ts/vue/single_organism.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div>
+
+        <table class="pure-table pure-table-horizontal">
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Genus</th>
+                <th>Species</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>{{ organism.id }}</td>
+                <td>{{ organism.genus }}</td>
+                <td>{{ organism.species }}</td>
+            </tr>
+            </tbody>
+        </table>
+
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped >
+</style>
+
+
+<script lang="ts">
+
+
+import {Vue, Component, Prop, Watch} from 'vue-property-decorator';
+import axios from 'axios';
+/**
+ * This component shows information about an organism
+ */
+@Component({})
+export default class SingleOrganismVue extends Vue {
+
+}
+</script>
