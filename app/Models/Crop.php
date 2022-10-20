1976ebb077cb9dee9c9641d8cd1419b819814438
Tsiory Arisoa <t.arisoa@mail.zeus.vision>
2022-10-20 19:09:32 +0200

    Added new Crop model and relations

---
+++ app/Models/Crop.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+
+/**
+ * Crop model representation
+ */
+class Crop extends Model
+{
+    use HasFactory;
+
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = self::TABLE;
+
+    public const TABLE = 'crops';
+
+    /**
+     * Id of the crop
+     * @var string
+     */
+    const  ID = 'id';
+
+    /**
+     * Name of the crop
+     * @var string
+     */
+    const NAME = 'name';
+
+
+    /**
+     * Crop relation with samples
+     * @return HasMany
+     */
+    public function crop(): HasMany
+    {
+        return $this->hasMany(Sample::class);
+    }
+}
--- app/Models/Sample.php
+++ app/Models/Sample.php
@@ -4,17 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
- * A sample
- * @property string $code The code to identify the sample
+ * Sample model representation
  */
 class Sample extends Model
 {
     use HasFactory;
 
-    public function abundances(){
+    /**
+     * Relation with abundances
+     * @return HasMany
+     */
+    public function abundances(): HasMany
+    {
         return $this->hasMany(Abundance::class);
+    }
+
+    /**
+     * Relation with crops
+     * @return BelongsTo
+     */
+    public function crop(): BelongsTo
+    {
+        return $this->belongsTo(Crop::class);
     }
 
 }
